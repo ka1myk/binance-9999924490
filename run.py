96e1f318c466d8a4a2c99f8eb8cae5dd2ac19e3a
ka1myk <kalmyk244kk>
2021-12-14 17:00:02 +0300

    12/14/21 17:00:01

--- run.py
+++ run.py
@@ -1,10 +1,7 @@
-import subprocess
+import os
+import time
 
-# Path to a Python interpreter that runs any Python script
-# under the virtualenv /path/to/virtualenv/
-python_bin = "/home/pi/Downloads/Binance-volatility-trading-bot-main/venv/bin"
-
-# Path to the script that must run under the virtualenv
-script_file = "/home/pi/Downloads/Binance-volatility-trading-bot-main/start.py"
-
-subprocess.Popen([python_bin, script_file])
+while True:
+    os.system("python3 start.py")
+    print("Restarting...")
+    time.sleep(5)
