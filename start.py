b5d16a8b8b297ecdb3b33a074098feb8ebadc926
ka1myk <kalmyk244kk>
2021-12-28 09:50:01 +0300

    12/28/21 09:50:01

--- start.py
+++ start.py
@@ -100,6 +100,7 @@ sys.stdout = St_ampe_dOut()
 
 def telegram_bot_sendtext(bot_message):
 
+
    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
    response = requests.get(send_text)
    return response.json()
