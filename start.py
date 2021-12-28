d1f60af282c43393748948bcc601a13fa75cd19a
ka1myk <kalmyk244kk>
2021-12-28 10:10:01 +0300

    12/28/21 10:10:01

--- start.py
+++ start.py
@@ -544,7 +544,7 @@ if __name__ == '__main__':
 
     # Load creds for correct environment
     access_key, secret_key = load_correct_creds(parsed_creds)
-
+    bot_token, bot_chatID = load_correct_creds(parsed_creds)
 
     if DEBUG:
         print(f'loaded config below\n{json.dumps(parsed_config, indent=4)}')
